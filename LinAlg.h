18161e9c47a8b2f3309f9d073d4d4129ce91aea9
Christopher Dyken <cdyk@users.noreply.github.com>
2021-12-28 10:28:10 +0100

    Generalize vector on type

--- LinAlg.h
+++ LinAlg.h
@@ -1,45 +1,71 @@
 #pragma once
 #include <cstddef>
 
-struct Vec2f
+/** 2D vector */
+template<typename T>
+struct Vec2
 {
-  Vec2f() = default;
-  Vec2f(const Vec2f&) = default;
-  Vec2f(float x) : x(x), y(x) {}
-  Vec2f(float* ptr) : x(ptr[0]), y(ptr[1]) {}
-  Vec2f(float x, float y) : x(x), y(y) {}
   union {
     struct {
-      float x;
-      float y;
+      T x;
+      T y;
     };
-    float data[2];
+    T data[2];
   };
-  float& operator[](size_t i) { return data[i]; }
-  const float& operator[](size_t i) const { return data[i]; }
+  T& operator[](size_t i) { return data[i]; }
+  const T& operator[](size_t i) const { return data[i]; }
 };
+typedef Vec2<float>    Vec2f;
+typedef Vec2<double>   Vec2d;
+typedef Vec2<int>      Vec2i;
+typedef Vec2<unsigned> Vec2u;
+template<typename T> Vec2<T> makeVec2(T x, T y) {   Vec2<T> u; u.x = x;    u.y = y;    return u; }
+template<typename T> Vec2<T> makeVec2(const T* p) { Vec2<T> u; u.x = p[0]; u.y = p[1]; return u; }
 
-
-struct Vec3f
+/** 3D vector */
+template<typename T>
+struct Vec3
 {
-  Vec3f() = default;
-  Vec3f(const Vec3f&) = default;
-  Vec3f(float x) : x(x), y(x), z(x) {}
-  Vec3f(float* ptr) : x(ptr[0]), y(ptr[1]), z(ptr[2]) {}
-  Vec3f(float x, float y, float z) : x(x), y(y), z(z) {}
-  Vec3f(const Vec2f& a, float z) : x(a.x), y(a.y), z(z) {}
-
   union {
     struct {
-      float x;
-      float y;
-      float z;
+      T x;
+      T y;
+      T z;
     };
-    float data[3];
+    T data[3];
   };
-  float& operator[](size_t i) { return data[i]; }
-  const float& operator[](size_t i) const { return data[i]; }
+  T& operator[](size_t i) { return data[i]; }
+  const T& operator[](size_t i) const { return data[i]; }
 };
+typedef Vec3<float>    Vec3f;
+typedef Vec3<double>   Vec3d;
+typedef Vec3<int>      Vec3i;
+typedef Vec3<unsigned> Vec3u;
+template<typename T> Vec3<T> makeVec3(T x, T y, T z) { Vec3<T> u; u.x = x;    u.y = y;    u.z = z;    return u; }
+template<typename T> Vec3<T> makeVec3(const T* p) {    Vec3<T> u; u.x = p[0]; u.y = p[1]; u.z = p[2]; return u; }
+
+/** 4D vector */
+template<typename T>
+struct Vec4
+{
+  union {
+    struct {
+      T x;
+      T y;
+      T z;
+      T w;
+    };
+    T data[4];
+  };
+  T& operator[](size_t i) { return data[i]; }
+  const T& operator[](size_t i) const { return data[i]; }
+};
+typedef Vec4<float>    Vec4f;
+typedef Vec4<double>   Vec4d;
+typedef Vec4<int>      Vec4i;
+typedef Vec4<unsigned> Vec4u;
+template<typename T> Vec4<T> makeVec4(T x, T y, T z, T w) { Vec4<T> u; u.x = x;    u.y = y;    u.z = z;    u.w = w;    return u; }
+template<typename T> Vec4<T> makeVec4(const T* p) {         Vec4<T> u; u.x = p[0]; u.y = p[1]; u.z = p[2]; u.w = p[3]; return u; }
 
 
 struct BBox3f
@@ -118,3 +144,67 @@ struct Mat3x4f
     float data[4 * 3];
   };
 };
+
+
+inline Vec2f makeVec2f(float x)                { return makeVec2(x, x); }
+inline Vec2f makeVec2f(float x, float y)       { return makeVec2(x, y); }
+inline Vec2f makeVec2f(const float* p)         { return makeVec2(p); }
+inline Vec2f makeVec2f(const Vec2f& v)         { return makeVec2(v.data); }
+inline Vec2f makeVec2f(const Vec3f& v)         { return makeVec2(v.data); }
+inline Vec2f makeVec2f(const Vec4f& v)         { return makeVec2(v.data); }
+inline Vec2d makeVec2d(double x)               { return makeVec2(x, x); }
+inline Vec2d makeVec2d(double x, double y)     { return makeVec2(x, y); }
+inline Vec2d makeVec2d(const double* p)        { return makeVec2(p); }
+inline Vec2d makeVec2d(const Vec2d& v)         { return makeVec2(v.data); }
+inline Vec2d makeVec2d(const Vec3d& v)         { return makeVec2(v.data); }
+inline Vec2d makeVec2d(const Vec4d& v)         { return makeVec2(v.data); }
+inline Vec2i makeVec2i(int x)                  { return makeVec2(x, x); }
+inline Vec2i makeVec2i(int x, int y)           { return makeVec2(x, y); }
+inline Vec2i makeVec2i(const int* p)           { return makeVec2(p); }
+inline Vec2i makeVec2i(const Vec2i& v)         { return makeVec2(v.data); }
+inline Vec2i makeVec2i(const Vec3i& v)         { return makeVec2(v.data); }
+inline Vec2i makeVec2i(const Vec4i& v)         { return makeVec2(v.data); }
+inline Vec2u makeVec2u(unsigned x)             { return makeVec2(x, x); }
+inline Vec2u makeVec2u(unsigned x, unsigned y) { return makeVec2(x, y); }
+inline Vec2u makeVec2u(const unsigned* p)      { return makeVec2(p); }
+inline Vec2u makeVec2u(const Vec2u& v)         { return makeVec2(v.data); }
+inline Vec2u makeVec2u(const Vec3u& v)         { return makeVec2(v.data); }
+inline Vec2u makeVec2u(const Vec4u& v)         { return makeVec2(v.data); }
+
+inline Vec3f makeVec3f(float x)                            { return makeVec3(x, x, x); }
+inline Vec3f makeVec3f(float x, float y, float z)          { return makeVec3(x, y, z); }
+inline Vec3f makeVec3f(const float* p)                     { return makeVec3(p); }
+inline Vec3f makeVec3f(const Vec3f& v)                     { return makeVec3(v.data); }
+inline Vec3f makeVec3f(const Vec4f& v)                     { return makeVec3(v.data); }
+inline Vec3d makeVec3d(double x)                           { return makeVec3(x, x, x); }
+inline Vec3d makeVec3d(double x, double y, double z)       { return makeVec3(x, y, z); }
+inline Vec3d makeVec3d(const double* p)                    { return makeVec3(p); }
+inline Vec3d makeVec3d(const Vec3d& v)                     { return makeVec3(v.data); }
+inline Vec3d makeVec3d(const Vec4d& v)                     { return makeVec3(v.data); }
+inline Vec3i makeVec3i(int x)                              { return makeVec3(x, x, x); }
+inline Vec3i makeVec3i(int x, int y, int z)                { return makeVec3(x, y, z); }
+inline Vec3i makeVec3i(const int* p)                       { return makeVec3(p); }
+inline Vec3i makeVec3i(const Vec3i& v)                     { return makeVec3(v.data); }
+inline Vec3i makeVec3i(const Vec4i& v)                     { return makeVec3(v.data); }
+inline Vec3u makeVec3u(unsigned x)                         { return makeVec3(x, x, x); }
+inline Vec3u makeVec3u(unsigned x, unsigned y, unsigned z) { return makeVec3(x, y, z); }
+inline Vec3u makeVec3u(const unsigned* p)                  { return makeVec3(p); }
+inline Vec3u makeVec3u(const Vec3u& v)                     { return makeVec3(v.data); }
+inline Vec3u makeVec3u(const Vec4u& v)                     { return makeVec3(v.data); }
+
+inline Vec4f makeVec4f(float x)                                        { return makeVec4(x, x, x, x); }
+inline Vec4f makeVec4f(float x, float y, float z, float w)             { return makeVec4(x, y, z, w); }
+inline Vec4f makeVec4f(const float* p)                                 { return makeVec4(p); }
+inline Vec4f makeVec4f(const Vec4f& v)                                 { return makeVec4(v.data); }
+inline Vec4d makeVec4d(double x)                                       { return makeVec4(x, x, x, x); }
+inline Vec4d makeVec4d(double x, double y, double z, double w)         { return makeVec4(x, y, z, w); }
+inline Vec4d makeVec4d(const double* p)                                { return makeVec4(p); }
+inline Vec4d makeVec4d(const Vec4d& v)                                 { return makeVec4(v.data); }
+inline Vec4i makeVec4i(int x)                                          { return makeVec4(x, x, x, x); }
+inline Vec4i makeVec4i(int x, int y, int z, int w)                     { return makeVec4(x, y, z, w); }
+inline Vec4i makeVec4i(const int* p)                                   { return makeVec4(p); }
+inline Vec4i makeVec4i(const Vec4i& v)                                 { return makeVec4(v.data); }
+inline Vec4u makeVec4u(unsigned x)                                     { return makeVec4(x, x, x, x); }
+inline Vec4u makeVec4u(unsigned x, unsigned y, unsigned z, unsigned w) { return makeVec4(x, y, z, w); }
+inline Vec4u makeVec4u(const unsigned* p)                              { return makeVec4(p); }
+inline Vec4u makeVec4u(const Vec4u& v)                                 { return makeVec4(v.data); }
--- LinAlgOps.h
+++ LinAlgOps.h
@@ -3,53 +3,179 @@
 #include <cfloat>
 #include "LinAlg.h"
 
-inline Vec2f operator*(const float a, const Vec2f& b) { return Vec2f(a*b.x, a*b.y); }
-
-inline Vec2f operator-(const Vec2f& a, const Vec2f& b) { return Vec2f(a.x - b.x, a.y - b.y); }
-
-inline Vec2f operator+(const Vec2f& a, const Vec2f& b) { return Vec2f(a.x + b.x, a.y + b.y); }
-
-inline Vec3f cross(const Vec3f& a, const Vec3f& b)
+template<typename T>
+Vec2<T> operator*(const T a, const Vec2<T>& b)
 {
-  return Vec3f(a.y * b.z - a.z * b.y,
-               a.z * b.x - a.x * b.z,
-               a.x * b.y - a.y * b.x);
+  return makeVec2<T>(a * b.x,
+                     a * b.y);
 }
 
-inline float dot(const Vec3f& a, const Vec3f& b) {
-  return a.x * b.x + a.y * b.y + a.z * b.z;
+template<typename T>
+Vec2<T> operator-(const Vec2<T>& a, const Vec2<T>& b)
+{
+  return makeVec2<T>(a.x - b.x,
+                     a.y - b.y);
 }
 
-inline Vec3f operator+(const Vec3f& a, const Vec3f& b) { return Vec3f(a.x + b.x, a.y + b.y, a.z + b.z); }
-
-inline Vec3f operator-(const Vec3f& a, const Vec3f& b) { return Vec3f(a.x - b.x, a.y - b.y, a.z - b.z); }
-
-inline Vec3f operator*(const float a, const Vec3f& b) { return Vec3f(a*b.x, a*b.y, a*b.z); }
-
-inline float lengthSquared(const Vec3f& a) { return dot(a, a); }
-
-inline float length(const Vec3f& a) { return std::sqrt(dot(a, a)); }
-
-inline float distanceSquared(const Vec3f& a, const Vec3f&b) { return lengthSquared(a - b); }
-
-inline float distance(const Vec3f& a, const Vec3f&b) { return length(a - b); }
-
-inline Vec3f normalize(const Vec3f& a) { return (1.f / length(a))*a; }
-
-inline void write(float* dst, const Vec3f& a) { *dst++ = a.data[0]; *dst++ = a.data[1]; *dst++ = a.data[2]; }
-
-inline Vec3f max(const Vec3f& a, const Vec3f& b)
+template<typename T>
+Vec2<T> operator+(const Vec2<T>& a, const Vec2<T>& b)
 {
-  return Vec3f(a.x > b.x ? a.x : b.x,
-               a.y > b.y ? a.y : b.y,
-               a.z > b.z ? a.z : b.z);
+  return makeVec2<T>(a.x + b.x,
+                     a.y + b.y);
+}
+
+template<typename T>
+Vec3<T> operator*(const T a, const Vec3<T>& b)
+{
+  return makeVec3<T>(a * b.x,
+                     a * b.y,
+                     a * b.z);
+}
+
+template<typename T>
+Vec3<T> operator-(const Vec3<T>& a, const Vec3<T>& b)
+{
+  return makeVec3<T>(a.x - b.x,
+                     a.y - b.y,
+                     a.z - b.z);
+}
+
+template<typename T>
+Vec3<T> operator+(const Vec3<T>& a, const Vec3<T>& b)
+{
+  return makeVec3<T>(a.x + b.x,
+                     a.y + b.y,
+                     a.z + b.z);
+}
+
+template<typename T>
+Vec4<T> operator*(const T a, const Vec4<T>& b)
+{
+  return makeVec4<T>(a * b.x,
+                     a * b.y,
+                     a * b.z,
+                     a * b.w);
+}
+
+template<typename T>
+Vec4<T> operator-(const Vec4<T>& a, const Vec4<T>& b)
+{
+  return makeVec4<T>(a.x - b.x,
+                     a.y - b.y,
+                     a.z - b.z,
+                     a.w - b.w);
+}
+
+template<typename T>
+Vec4<T> operator+(const Vec4<T>& a, const Vec4<T>& b)
+{
+  return makeVec4<T>(a.x + b.x,
+                     a.y + b.y,
+                     a.z + b.z,
+                     a.w + b.w);
+}
+
+template<typename T> Vec3<T> cross(const Vec3<T>& a, const Vec3<T>& b)
+{
+  return makeVec3<T>(a.y * b.z - a.z * b.y,
+                     a.z * b.x - a.x * b.z,
+                     a.x * b.y - a.y * b.x);
+}
+
+template<typename T> T dot(const Vec2<T>& a, const Vec2<T>& b) { return a.x * b.x + a.y * b.y; }
+template<typename T> T dot(const Vec3<T>& a, const Vec3<T>& b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
+template<typename T> T dot(const Vec4<T>& a, const Vec4<T>& b) { return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w; }
+
+template<typename T> T lengthSquared(const Vec2<T>& a) { return dot(a, a); }
+template<typename T> T lengthSquared(const Vec3<T>& a) { return dot(a, a); }
+template<typename T> T lengthSquared(const Vec4<T>& a) { return dot(a, a); }
+
+template<typename T> T distanceSquared(const Vec2<T>& a, const Vec2<T>&b) { return lengthSquared(a - b); }
+template<typename T> T distanceSquared(const Vec3<T>& a, const Vec3<T>&b) { return lengthSquared(a - b); }
+template<typename T> T distanceSquared(const Vec4<T>& a, const Vec4<T>&b) { return lengthSquared(a - b); }
+
+template<typename T> T length(const Vec2<T>& a) { return std::sqrt(dot(a, a)); }
+template<typename T> T length(const Vec3<T>& a) { return std::sqrt(dot(a, a)); }
+template<typename T> T length(const Vec4<T>& a) { return std::sqrt(dot(a, a)); }
+
+template<typename T> T distance(const Vec2<T>& a, const Vec2<T>&b) { return length(a - b); }
+template<typename T> T distance(const Vec3<T>& a, const Vec3<T>&b) { return length(a - b); }
+template<typename T> T distance(const Vec4<T>& a, const Vec4<T>&b) { return length(a - b); }
+
+template<typename T> Vec2<T> normalize(const Vec2<T>& a) { return (T(1) / length(a)) * a; }
+template<typename T> Vec3<T> normalize(const Vec3<T>& a) { return (T(1) / length(a)) * a; }
+template<typename T> Vec4<T> normalize(const Vec4<T>& a) { return (T(1) / length(a)) * a; }
+
+template<typename T>
+T* write(T* dst, const Vec2<T>& a)
+{
+  *dst++ = a.data[0];
+  *dst++ = a.data[1];
+  return dst;
+}
+
+template<typename T>
+T* write(T* dst, const Vec3<T>& a)
+{
+  *dst++ = a.data[0];
+  *dst++ = a.data[1];
+  *dst++ = a.data[2];
+  return dst;
+}
+
+template<typename T>
+T* write(T* dst, const Vec4<T>& a)
+{
+  *dst++ = a.data[0];
+  *dst++ = a.data[1];
+  *dst++ = a.data[2];
+  *dst++ = a.data[3];
+  return dst;
+}
+
+template<typename T>
+Vec2<T> max(const Vec2<T>& a, const Vec2<T>& b)
+{
+  return makeVec2<T>(a.x > b.x ? a.x : b.x,
+                     a.y > b.y ? a.y : b.y);
+}
+
+template<typename T>
+Vec3<T> max(const Vec3<T>& a, const Vec3<T>& b)
+{
+  return makeVec3<T>(a.x > b.x ? a.x : b.x,
+                     a.y > b.y ? a.y : b.y,
+                     a.z > b.z ? a.z : b.z);
+}
+
+template<typename T>
+Vec4<T> max(const Vec4<T>& a, const Vec4<T>& b)
+{
+  return makeVec4<T>(a.x > b.x ? a.x : b.x,
+                     a.y > b.y ? a.y : b.y,
+                     a.z > b.z ? a.z : b.z,
+                     a.w > b.w ? a.w : b.w);
+}
+
+inline Vec2f min(const Vec2f& a, const Vec2f& b)
+{
+  return makeVec2f(a.x < b.x ? a.x : b.x,
+                   a.y < b.y ? a.y : b.y);
 }
 
 inline Vec3f min(const Vec3f& a, const Vec3f& b)
 {
-  return Vec3f(a.x < b.x ? a.x : b.x,
-               a.y < b.y ? a.y : b.y,
-               a.z < b.z ? a.z : b.z);
+  return makeVec3f(a.x < b.x ? a.x : b.x,
+                   a.y < b.y ? a.y : b.y,
+                   a.z < b.z ? a.z : b.z);
+}
+
+inline Vec4f min(const Vec4f& a, const Vec4f& b)
+{
+  return makeVec4f(a.x < b.x ? a.x : b.x,
+                   a.y < b.y ? a.y : b.y,
+                   a.z < b.z ? a.z : b.z,
+                   a.w < b.w ? a.w : b.w);
 }
 
 Mat3f inverse(const Mat3f& M);
@@ -81,12 +207,13 @@ inline Vec3f mul(const Mat3x4f& A, const Vec3f& x)
 
 inline BBox3f createEmptyBBox3f()
 {
-  return BBox3f(Vec3f(FLT_MAX, FLT_MAX, FLT_MAX), Vec3f(-FLT_MAX, -FLT_MAX, -FLT_MAX));
+  return BBox3f(makeVec3f(FLT_MAX, FLT_MAX, FLT_MAX),
+                makeVec3f(-FLT_MAX, -FLT_MAX, -FLT_MAX));
 }
 
 inline BBox3f::BBox3f(const BBox3f& bbox, float margin) :
-  min(bbox.min - Vec3f(margin)),
-  max(bbox.max + Vec3f(margin))
+  min(bbox.min - makeVec3f(margin)),
+  max(bbox.max + makeVec3f(margin))
 {}
 
 inline void engulf(BBox3f& target, const Vec3f& p)
